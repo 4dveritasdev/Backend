e41d6f330eae0d91b48c213e9a6eb289bee184f1
Chris <dev@4dveritas.com>
2023-11-01 12:30:19 -0400

    implement multi processing for generating qr code

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,13 +2,15 @@ const Product = require('../models/productModel');
 const QRcode = require('../models/qrcodeModel');
 const base = require('./baseController');
 const APIFeatures = require('../utils/apiFeatures');
-const { generateQrCode } = require('../utils/generateQrCode');
-const { initClient, batchMint, getNonce } = require('../web3/index.ts');
+// const { generateQrCode } = require('../utils/generateQrCode');
 const { Worker, isMainThread, parentPort, workerData } = require('worker_threads');
+// require('ts-node').register();
+const { initClient, batchMint, getNonce } = require('../web3/index.ts');
 // const qr = require('qr-image');
-const qr = require('qrcode')
+const qrcode = require('qrcode')
 
-const divcount = 10;
+const divcount = 10000;
+const numThreads = 4;
 
 exports.getAllProducts = async(req, res, next) => {
     try {
@@ -68,43 +70,85 @@ exports.mint = async(req, res, next) => {
 
         let qrcodeIds = [];
         console.log(product);
-        console.log(product.contract_address, req.body.amount);
-        for(let i = 1; i <= req.body.amount; i ++) {
-            let stringdata = JSON.stringify({
-                company_id: product.company_id,
-                product_id: product._id,
-                token_id: product.total_minted_amount + i
-            })
+        let start = new Date();
 
-            qr.toDataURL(stringdata, async function (err, code) {
-                if(err) return console.log("error occurred")
+        const qrCodePromises = [];
 
-                const newQrCode = await QRcode.create({
-                    product_id: product._id,
-                    image: code
-                });
-                qrcodeIds.push((newQrCode._id).toString());
-                if(qrcodeIds.length == req.body.amount) {
-                    // console.log(qrcodeIds);
-                    console.log(qrcodeIds.length / divcount);
-                    batchMint(product.contract_address, qrcodeIds.slice((qrcodeIds.length / divcount - 1) * divcount));
-                }
-                else if(qrcodeIds.length % divcount === 0) {
-                    console.log(qrcodeIds.length / divcount);
-                    batchMint(product.contract_address, qrcodeIds.slice((qrcodeIds.length / divcount - 1) * divcount, divcount));
-                }
-            })
+        for(let j = 0; j < numThreads; j ++) {
+            qrCodePromises.push(
+                new Promise((resolve, reject) => {
+                    const worker = new Worker(__filename, { execArgv: /\.ts$/.test(__filename) ? ["--require", "ts-node/register"] : undefined });
+                    worker.on('message', async (data) => {
+                        const { code } = data;
+                        const newQrCode = await QRcode.create({
+                            product_id: product._id,
+                            image: code
+                        });
+
+                        qrcodeIds.push((newQrCode._id).toString());
+                        if (qrcodeIds.length === req.body.amount) {
+                            // batchMint(product.contract_address, qrcodeIds);
+                            // console.log(qrcodeIds);
+                            // if(req.body.amount > divcount) {
+                            console.log('last');
+                            await batchMint(product.contract_address, qrcodeIds.slice((qrcodeIds.length / divcount - 1) * divcount));
+                            // } else {
+                            //     await batchMint(product.contract_address, qrcodeIds);
+                            // }
+                            let end = new Date();
+                            console.log(end.getTime() - start.getTime())
+                            res.status(200).json({
+                                status: 'success',
+                                offset: product.total_minted_amount,
+                            });
+                        } else if(qrcodeIds.length % divcount === 0) {
+                            console.log(qrcodeIds.length / divcount);
+                            batchMint(product.contract_address, qrcodeIds.slice((qrcodeIds.length / divcount - 1) * divcount, divcount));
+                        }
+                        resolve();
+                    });
+                    
+                    worker.on('error', (error) => { console.log(error), reject(error)});
+
+                    for (let i = 1; i <= req.body.amount / numThreads; i ++) {
+                        worker.postMessage({ threadNumber: j, product, i });
+                    }
+                })
+            );
         }
 
-        res.status(200).json({
-            status: 'success',
-            offset: product.total_minted_amount
-            // data: {
-            //     doc
-            // }
-        });
+        await Promise.all(qrCodePromises);
 
     } catch (error) {
         next(error);
     }
 };
+
+if (!isMainThread) {
+    // Worker thread code for generating a QR code
+    parentPort.on('message', async (data) => {
+        const { product, i, threadNumber } = data;
+        const stringdata = JSON.stringify({
+            company_id: product.company_id,
+            product_id: product._id,
+            token_id: product.total_minted_amount + i + threadNumber * numThreads,
+        });
+    
+        const code = await generateQRCode(stringdata);
+        
+        // Send the generated QR code back to the main thread
+        parentPort.postMessage({ code: code });
+    });
+  }
+  
+const generateQRCode = (data) => {
+    return new Promise((resolve, reject) => {
+        qrcode.toDataURL(data, (err, code) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(code);
+            }
+        });
+    });
+}
