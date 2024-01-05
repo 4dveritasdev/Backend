5c89bee878827888e071e5f7c973b9bff28aa389
Christopher Groe <dev@4dveritas.com>
2024-01-05 18:12:39 -0500

    Update app.ts

--- app.ts
+++ app.ts
@@ -40,7 +40,7 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({
-    limit: '100kb'
+    limit: '100mb'
 }));
 
 // Data sanitization against Nosql query injection
