88766d524539fbec48795c7b65b84a64f59501a9
4dveritasdev <dev@4dveritas.com>
2024-10-04 04:18:34 -0400

    Update Serials Route'

--- routes/qrcodeRoutes.ts
+++ routes/qrcodeRoutes.ts
@@ -8,10 +8,10 @@ const QRcodeController = require('../controllers/qrcodeController');
 router.get('/', QRcodeController.getAllQRcodes);
 router.post('/product', QRcodeController.getQRcodesWithProductId);
 router.post('/decrypt', QRcodeController.decrypt);
-router.get('/:id', QRcodeController.getQRcode);
-router.post('/', QRcodeController.addQRcode);
 router.post('/serialdata/productinfo',QRcodeController.getProductInfoWithSerial);
 router.get('/serials',QRcodeController.getSerials)
+router.get('/:id', QRcodeController.getQRcode);
+router.post('/', QRcodeController.addQRcode);
 router.put('/:id', QRcodeController.updateQRcode);
 router.delete('/:id', QRcodeController.deleteQRcode);
 router.get('/:id/productinfo', QRcodeController.getProductInfoWithQRCodeID);
