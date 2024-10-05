5435099390065def087f841427736e95a984a4b8
4dveritasdev <dev@4dveritas.com>
2024-10-04 22:55:14 -0400

    Update Route

--- routes/qrcodeRoutes.ts
+++ routes/qrcodeRoutes.ts
@@ -9,7 +9,7 @@ router.get('/', QRcodeController.getAllQRcodes);
 router.post('/product', QRcodeController.getQRcodesWithProductId);
 router.post('/decrypt', QRcodeController.decrypt);
 router.post('/serialdata/productinfo',QRcodeController.getProductInfoWithSerial);
-router.get('/serials',QRcodeController.getSerials)
+router.post('/serials',QRcodeController.getSerials)
 router.get('/:id', QRcodeController.getQRcode);
 router.post('/', QRcodeController.addQRcode);
 router.put('/:id', QRcodeController.updateQRcode);
