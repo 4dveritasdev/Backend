66233803b784dc43cf3beb7e3cf5473225f58aba
4dveritasdev <dev@4dveritas.com>
2024-11-10 08:51:15 -0500

    Update route

--- routes/companyRoutes.ts
+++ routes/companyRoutes.ts
@@ -7,13 +7,14 @@ const CompanyController = require('../controllers/companyController');
 
 router.get('/', CompanyController.getAllCompanys);
 router.post('/auth', CompanyController.login);
+router.get('/products',CompanyController.getProductsByCompanyId);
 // router.get('/info/:wallet', CompanyController.getCompanyByWallet);
 router.get('/:id', CompanyController.getCompany);
 router.post('/', CompanyController.addCompany);
 router.put('/:id', CompanyController.updateCompany);
 router.delete('/:id', CompanyController.deleteCompany);
 router.get('/verify/:id', CompanyController.verify);
-router.get('/products',CompanyController.getProductsByCompanyId);
+
 
 
 module.exports = router;
